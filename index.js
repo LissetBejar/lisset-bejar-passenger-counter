cefc6a2c359a6f9bb209b07143b1bf5f3bfff5fb
Bejar L <143468066+BejarL@users.noreply.github.com>
2024-01-03 13:58:29 -0800

    Finished project

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-// document.getElementById("count-el").innerText = 5.innerText = 5
-
-// change the count-el in the HTML to reflect the new count
-
-// camelCase
-let countEl = document.getElementById("count-el") // pass in arguments
-
 let count = 0
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
 
 function increment() {
-  count = count + 1
-  countEl.innerText = count
+  count += 1
+  countEl.textContent = count
+}
+
+function save() {
+  let entries = count + " - "
+  saveEl.textContent += entries
+  countEl.textContent = 0
+  count = 0
 }
